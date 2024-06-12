0ec1ef45f124324565f2f8ed194c3321333844c0
Lukas Adam <ady709@seznam.cz>
2024-06-12 17:59:47 +0200

    0.0.3 -it's starting to resemble a tetris -next piece is shown -game over is detected

--- tetrisModel.py
+++ tetrisModel.py
@@ -1,5 +1,6 @@
-from random import random
-
+import random
+from datetime import datetime
+random.seed(datetime.now().microsecond)
 
 #objects: tri, square, i, s, rs, l, j
 
@@ -8,6 +9,7 @@ class Object:
         self.c = c
         self.r = r
         if shape is None:
+            print('Shape is None')
             raise AttributeError
         if shape == 'tri':
             self.cmap = [['gold4', None],
@@ -17,7 +19,7 @@ class Object:
             self.cmap = [['blue', 'blue'],
                          ['blue', 'blue']]
         elif shape == 'i':
-            self.cmap = [['red', 'red', 'red', 'red']]
+            self.cmap = [['red'], ['red'], ['red'], ['red']]
         elif shape == 's':
             self.cmap = [['green2', None],
                          ['green2', 'green2'],
@@ -41,6 +43,7 @@ class Object:
         #construct from cmap
         elif type(shape) == list:
             if type(shape[0]) != list:
+                print('Shape[0] is not list')
                 raise AttributeError
             self.cmap = shape
 
@@ -74,11 +77,6 @@ class Object:
         leftmin = min([item for item in self.leftmost if item is not None])
         rightmax = max([item for item in self.rightmost if item is not None])
         width = rightmax - leftmin
-        #TODO?? self.c = self.c - int(width)/2
-
-        print(f'L:{self.leftmost}')
-        print(f'R:{self.rightmost}')
-        print(f'B:{self.bottom}')
 
     def getRotatedCmap(self):
         cmap = [[]]
@@ -95,7 +93,7 @@ class Object:
 
 
 class TetrisModel:
-    probability = {'tri': 20, 'square': 40, 'i': 50, 's': 60, 'rs': 70, 'l': 80, 'j': 100}
+    shapes = {0:'tri', 1:'square', 2:'i', 3:'s', 4:'rs', 5:'l', 6:'j'}
 
     def __init__(self, rows, columns):
         self.rows = rows
@@ -103,12 +101,29 @@ class TetrisModel:
         self.playArea = [[None for i in range(0, columns)] for o in range(0, rows)]
         self.addObject = True
         self.landed = False
+        self.completeRows = []
+        self.nextObject = Object(shape=self.getRandomObject())
+        self.gameOver = False
 
     def tick(self):
+        if self.gameOver:
+            return
         if self.addObject:
             #add new object if needed
-            self.object = Object(shape=self.getRandomObject(), r=0, c=int(self.columns / 2))
+            self.object = Object(shape=self.nextObject.cmap, r=0, c=int(self.columns / 2))
+            self.nextObject = Object(shape=self.getRandomObject())
             self.addObject = False
+            #check for game over
+            gameOver=False
+            for y,row in enumerate(self.object.cmap):
+                for x,block in enumerate(row):
+                    if self.object.cmap[y][x] and self.playArea[self.object.r+y][self.object.c+x]:
+                        gameOver = True
+                        break
+                    if gameOver:
+                        break
+            if gameOver:
+                self.gameOver = True
             return
 
         #see if the objact has landed
@@ -124,13 +139,18 @@ class TetrisModel:
                         self.playArea[self.object.r + y][self.object.c + x] = block
             self.addObject = True
             self.landed = False
+            self.checkCompleteRows()
+            self.removeCompleteRows()
+            self.tick()
 
         #fall the object down
         if not self.landed and not self.addObject:
             self.object.r += 1
-        print(f'xpos:{self.object.c} ypos:{self.object.r}')
+    #end of tick
 
     def goLeft(self):
+        if self.gameOver:
+            return
         cando = True
         for r, x in enumerate(self.object.leftmost):
             if not x is None:
@@ -141,8 +161,9 @@ class TetrisModel:
                         cando = False
         if cando:
             self.object.c -= 1
-        print(f'xpos:{self.object.c} ypos:{self.object.r}')
     def goRight(self):
+        if self.gameOver:
+            return
         cando = True
         for r, x in enumerate(self.object.rightmost):
             if not x is None:
@@ -153,14 +174,14 @@ class TetrisModel:
                         cando = False
         if cando:
             self.object.c += 1
-        print(f'xpos:{self.object.c} ypos:{self.object.r}')
 
     def rotate(self):
+        if self.gameOver:
+            return
         rotatedObject = self.object.getRotatedObject()
         shift = (self.object.width - rotatedObject.width) / 2
         addition = 0.5 if shift > 0 else -0.5
         rotatedObject.c += (int(shift + addition))
-        print(f'rotated object x:{rotatedObject.c} y:{rotatedObject.r}')
         #see if rotated object fits in the playArea
         #tryto shift it if it does not to see if it can be rotated afterwards
         #tupple shift (column,row)
@@ -171,11 +192,9 @@ class TetrisModel:
             canRotate = True
             rotatedObject.r = first_r + sh[1]
             rotatedObject.c = first_c + sh[0]
-            print(f'shifting {sh}')
             if rotatedObject.r + rotatedObject.height > self.rows\
             or rotatedObject.c < 0\
             or rotatedObject.c + rotatedObject.width > self.columns:
-                print(f'ng {rotatedObject.c} {rotatedObject.r}')
                 canRotate = False
                 continue
             for y, r in enumerate(rotatedObject.cmap):
@@ -188,12 +207,28 @@ class TetrisModel:
             if canRotate:
                 break
         if canRotate:
-            print(f'Rotated, new pos x:{rotatedObject.c} y:{rotatedObject.r}')
             self.object = rotatedObject
 
     def getRandomObject(self):
-        r = int(random() * 100)
-        for k, v in self.probability.items():
-            if r < v:
-                print(k)
-                return k
+        return self.shapes[int(random.random() * 1000) % 7]
+
+
+    def checkCompleteRows(self):
+        self.completeRows = []
+        for r,row in enumerate(self.playArea):
+            compl = True
+            for block in row:
+                if not block:
+                    compl = False
+                    break
+            if compl:
+                self.completeRows.append(r)
+
+
+    def removeCompleteRows(self):
+        if not len(self.completeRows):
+            return
+        for r in self.completeRows:
+            del(self.playArea[r])
+            self.playArea.insert(0,[None for item in range(0,self.columns)])
+
--- tetrisView.py
+++ tetrisView.py
@@ -12,17 +12,40 @@ class TetrisView:
         self.canH = blockSize*rows
         self.canW = blockSize*columns
 
+        #playArea
         self.frame = ttk.Frame(root, borderwidth=2, relief='ridge')
-        self.frame.pack()
+        self.frame.grid(row=1, column=1, rowspan=2)
 
         self.can = tk.Canvas(self.frame, width=self.canW + 1, height=self.canH + 1, background='darkgrey')
         self.can.pack()
         self.can.config(scrollregion=(0, 0, self.canW, self.canH))
 
+        #nextPiece
+        self.frameNext = ttk.Frame(root, borderwidth=0, relief = 'ridge')
+        self.frameNext.grid(row=1, column=2, sticky='n')
+        self.canNextW = self.canNextH = self.blockSize*5
+        self.canNext = tk.Canvas(self.frameNext, width=self.canNextW, height=self.canNextH)
+        self.canNext.pack()
+        self.canNext.config(scrollregion=(0, 0, self.canNextW, self.canNextH))
+
     def updateView(self):
+
+        def drawObject(obj, canv):
+            for y in range(0,len(obj.cmap)):
+                for x in range(0,len(obj.cmap[y])):
+                    xpos = (obj.c)*self.blockSize+x*self.blockSize
+                    ypos = obj.r * self.blockSize+y*self.blockSize
+                    blockColor = obj.cmap[y][x]
+                    if blockColor:
+                        canv.create_rectangle(xpos, ypos, xpos + self.blockSize, ypos + self.blockSize,
+                                                  fill=blockColor)
+
+
         #clear all shapes
         for item in self.can.find_all():
             self.can.delete(item)
+        for item in self.canNext.find_all():
+            self.canNext.delete(item)
         #draw playfield
         for y,row in enumerate(self.model.playArea):
             for x,color in enumerate(row):
@@ -31,14 +54,10 @@ class TetrisView:
                     ypos = y*self.blockSize
                     self.can.create_rectangle(xpos, ypos, xpos+self.blockSize, ypos+self.blockSize, fill=color)
         #draw played object
-        for y in range(0,len(self.model.object.cmap)):
-            for x in range(0,len(self.model.object.cmap[y])):
-                xpos = (self.model.object.c)*self.blockSize+x*self.blockSize
-                ypos = self.model.object.r * self.blockSize+y*self.blockSize
-                clr = self.model.object.cmap[y][x]
-                if clr:
-                    self.can.create_rectangle(xpos, ypos, xpos + self.blockSize, ypos + self.blockSize,
-                                              fill=clr)
+        drawObject(self.model.object, self.can)
+
+        #draw next object
+        drawObject(self.model.nextObject, self.canNext)
 
     def keyInput(self,event):
         self.controller.keyInput(event)
